10d198e560fad6e03a6f46a130a1085cf30eb19b
Fellipe Lessa Mesquita <fellipelessa201@gmail.com>
2026-05-30 20:59:48 -0300

    Mudança no pom

--- src/main/java/com/universidade/dao/ConexaoDB.java
+++ src/main/java/com/universidade/dao/ConexaoDB.java
@@ -8,7 +8,7 @@ public class ConexaoDB {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/universidade";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String PASSWORD = "postgres123";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

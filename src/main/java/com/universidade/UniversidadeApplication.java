be6680e051ce4761a9a644b72f310dc785e637a9
Fellipe Lessa Mesquita <fellipelessa201@gmail.com>
2026-06-08 08:41:41 -0300

    Fix formatting in UniversidadeApplication class

--- src/main/java/com/universidade/UniversidadeApplication.java
+++ src/main/java/com/universidade/UniversidadeApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversidadeApplication {// v2
+public class UniversidadeApplication { // v2
 
     public static void main(String[] args) {
         SpringApplication.run(UniversidadeApplication.class, args);
         new TabelasDAO().criarTabelas();
     }
+}

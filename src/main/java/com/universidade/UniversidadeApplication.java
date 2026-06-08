43d8d21763c6e47a6151b6a2eedf4996ec389070
Fellipe Lessa Mesquita <fellipelessa201@gmail.com>
2026-06-08 08:37:27 -0300

    Update UniversidadeApplication.java

--- src/main/java/com/universidade/UniversidadeApplication.java
+++ src/main/java/com/universidade/UniversidadeApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversidadeApplication {
+public class UniversidadeApplication {// v2
 
     public static void main(String[] args) {
         SpringApplication.run(UniversidadeApplication.class, args);
         new TabelasDAO().criarTabelas();
     }
-}
